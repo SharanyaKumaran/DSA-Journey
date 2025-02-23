6cfa2f859a804c9204e0bab4829030eb456b8df3
Sharanya K R <151659113+SharanyaKumaran@users.noreply.github.com>
2025-02-23 17:11:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter:");
+        int n1=sc.nextInt();
+        sc.close();
+        switch(n1){
+        case 1:
+            System.out.print("Monday");
+            break;
+        case 2:
+            System.out.print("Tuesday");
+            break;
+        case 3:
+            System.out.print("Wednesday");
+            break;
+        case 4:
+            System.out.print("Thursday");
+            break;
+        case 5:
+            System.out.print("Friday");
+            break;
+        default:
+            System.out.print("Invalid");
+            break;
+        }
+    }
+}
