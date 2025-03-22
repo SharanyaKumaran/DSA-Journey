9424a88dc1d28b9df4de91ea1138b055f0135a06
Sharanya K R <151659113+SharanyaKumaran@users.noreply.github.com>
2025-03-22 20:53:54 +0530

    Add files via upload

---
+++ Week 5 Array,Sorting & STring/Anagram_str.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Anagram_str {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    String s=sc.next();
+    String s2=sc.next();
+    sc.close();
+    AnagramChecker(s,s2);
+  }
+  public static AnagramChecker(String s,String s2){
+
+  }
+}
